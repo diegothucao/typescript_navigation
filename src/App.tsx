2c690672279c72527851fa0c5d2c56afe860bd1f
Thu CAO <cao.trung.thu@gmail.com>
2019-09-21 19:21:19 +0700

    update realm

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import React from 'react'
+import { View, Text } from 'react-native'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
 class HomeScreen extends React.Component {
   render() {
