bff9ab5790a1f5ab926241d966926c7b68b3cd31
Thu CAO <cao.trung.thu@gmail.com>
2019-09-22 18:54:18 +0700

    update login screen

--- src/ui/login/Login.tsx
+++ src/ui/login/Login.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { observer, inject } from "mobx-react"
+import { Input, Button } from 'react-native-elements'
 
 export default class Login extends React.Component {
   static navigationOptions = {
@@ -9,27 +10,45 @@ export default class Login extends React.Component {
 
   render() {
     return (
-      <View style = {styles.mainArea} >
-        <View style={styles.loginArea}></View>
+      <View style={styles.mainArea} >
+        <View style={styles.loginArea}>
+          <Input
+            placeholder='Email' autoCompleteType = "email" keyboardType = "email-address" autoCapitalize = "none"
+          />
+          <Input
+            placeholder='Password' secureTextEntry={true} autoCompleteType = "password" autoCapitalize = "none"
+          />
+          <Button
+            title="Login" style={styles.loginButton}
+          />
+        </View>
+
       </View>
     )
   }
 }
 
-  const styles = StyleSheet.create({
-    mainArea: {
-      width : "100%",
-      height : "100%",
-      flexDirection: 'column',
-      justifyContent: 'center',
-      backgroundColor: "aliceblue",
-     
-    },
-    loginArea: {
-      height:  150,
-      marginLeft: 16,
-      marginRight: 16,
-      backgroundColor: "white"
-    }
-  })
+const styles = StyleSheet.create({
+  mainArea: {
+    width: "100%",
+    height: "100%",
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: "aliceblue",
+
+  },
+  loginArea: {
+    height: 200,
+    marginLeft: 16,
+    marginRight: 16,
+    flexDirection: 'column'
+  },
+
+  loginButton: {
+    width: 120,
+    marginTop: 40,
+    alignSelf: "center"
+  }
+
+})
 
