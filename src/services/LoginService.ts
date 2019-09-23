7b5416f686cf76315390feb52cabd593f3f8a8c7
Thu CAO <cao.trung.thu@gmail.com>
2019-09-23 16:44:43 +0700

    update cache

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -1,5 +1,5 @@
 import { create, ApisauceInstance } from 'apisauce'
-import { AsyncStorage } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage'
 import { LoginUserData, UUserData } from '../model/UserModel'
 
 class LoginService {
